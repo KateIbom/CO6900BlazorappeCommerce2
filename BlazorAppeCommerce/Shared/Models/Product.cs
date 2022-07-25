1176f226c3ffb1ca9448b8814ec69f68d682c7c8
Kate Turton <72076413+KateTurton@users.noreply.github.com>
2022-07-25 15:15:45 +0100

    Initial commit

---
+++ BlazorAppeCommerce/Shared/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppeCommerce.Shared.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        // The official name for the product that will 
+        // be displayed to the customer on the website
+        public string Name { get; set; }    
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Rating { get; set; }
+        public decimal SalePrice { get; set; }
+        public decimal OfferPrice { get; set; }
+        public double Weight { get; set; }
+        public WeightUnits WeightUnit { get; set; }
+
+        public int CurrentStock { get; set; }
+
+        public int OrderedStock { get; set; }
+
+
+    }
+}
